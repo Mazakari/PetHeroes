62c872494af896a2b610492068ad14bf65986143
Stupa7 <intertrassa@gmail.com>
2023-07-15 14:01:10 +0300

    Начал создавать комнату с огнём

---
+++ Assets/Scripts/Logic/Window/FireRoom/FireRoom.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FireRoom : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private BoxCollider2D _roomCollider;
+    [SerializeField] private Sprite[] _sprite;
+
+
+}
