6db5f32a62365ed22e3daff9b53c067910190a4a
Roman Baranov <romanbaranov1984@gmail.com>
2023-07-15 13:03:17 +0300

    Добавил подсчет очков и отключение пустого окна с жертвой

--- Assets/Scripts/Logic/Platform/VictimScores.cs
+++ Assets/Scripts/Logic/Platform/VictimScores.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class VictimScores : MonoBehaviour
 {
+    [SerializeField] private int _playerLayer;
     private int _totalScores = 0;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(Constants.PLAYER_TAG))
+        if (collision.gameObject.layer == _playerLayer)
         {
             VictimBasketsControl basketsControl = collision.gameObject.GetComponentInChildren<VictimBasketsControl>();
             if (basketsControl != null)
@@ -19,6 +20,8 @@ public class VictimScores : MonoBehaviour
                         Debug.Log($"Scores = {_totalScores}");
                     }
                 }
+
+                basketsControl.DeactivateBaskets();
             }
         }
     }
--- Assets/Scripts/Logic/Window/VictimsRoom.cs
+++ Assets/Scripts/Logic/Window/VictimsRoom.cs
@@ -4,29 +4,45 @@ public class VictimsRoom : MonoBehaviour
 {
     [SerializeField] private GameObject[] _victimsPrefabs;
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private BoxCollider2D _roomCollider;
 
     private Victim[] _victims;
     private int _currentVictimIndex = 0;
-    private int _previoustVictimIndex = 0;
     private Victim _currentVictim;
 
     private void OnEnable() => 
         InitRoom();
 
-    public Victim SaveCurrentVictim()
+    public Victim GetCurrentVictim() =>
+        _currentVictim;
+    public void ActivateNextVictim()
     {
-        ActivateNextVictim();
-        return _currentVictim;
+        DeactivateCurrentVictim();
+        _currentVictimIndex++;
+
+        if (_currentVictimIndex < _victims.Length)
+        {
+            ActivateCurrentVictim();
+        }
+        else
+        {
+            _roomCollider.enabled = false;
+        }
+    }
+
+    private void DeactivateCurrentVictim()
+    {
+        _victims[_currentVictimIndex].gameObject.SetActive(false);
+        SetRoomSprite(null);
     }
 
     private void InitRoom()
     {
         _currentVictimIndex = 0;
-        _previoustVictimIndex = _currentVictimIndex;
 
         InitVictimsArray();
         SpawnVictimsPrefabs();
-        ActivateNextVictim();
+        ActivateCurrentVictim();
     }
 
     private void InitVictimsArray()
@@ -52,32 +68,13 @@ public class VictimsRoom : MonoBehaviour
     }
     private void AddVictim(GameObject victim, int i) => 
         _victims[i] = victim.GetComponent<Victim>();
-    private void ActivateNextVictim()
-    {
-        DeactivatePreviousVictim();
-        ActivateCurrentVictim();
-
-        IncrementPreviousAndCurrentIndex();
-    }
-    private void DeactivatePreviousVictim()
-    {
-        Victim previousVictim = _victims[_previoustVictimIndex];
-        previousVictim.gameObject.SetActive(false);
-    }
-
+   
     private void ActivateCurrentVictim()
     {
         _currentVictim = _victims[_currentVictimIndex];
 
         _currentVictim.gameObject.SetActive(true);
         SetRoomSprite(_currentVictim.GetSprite());
-    }
-    private void IncrementPreviousAndCurrentIndex()
-    {
-        _previoustVictimIndex = _currentVictimIndex;
-        _currentVictimIndex++;
-
-        _currentVictimIndex = Mathf.Clamp(_currentVictimIndex, 0, _victims.Length - 1);
     }
 
     private void SetRoomSprite(Sprite sprite) =>
