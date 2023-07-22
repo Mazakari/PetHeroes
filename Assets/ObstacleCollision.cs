b7f0aaea1ea848abb29a2d223c54856e031c3355
Stupa7 <intertrassa@gmail.com>
2023-07-22 23:26:55 +0300

    Refactoring

--- Assets/ObstacleCollision.cs
+++ Assets/ObstacleCollision.cs
@@ -16,16 +16,29 @@ public class ObstacleCollision : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == _playerLayer)
-        if (_HP > 0)
-        {
-         _HP--;
-        }
-        else
-        {
-         _collider.enabled = false;
-        }
-        _sound.Play();
+        DecreaseHPandPlaySound(collision);
 
+    }
+
+    private void DecreaseHPandPlaySound(Collision2D collision)
+    {
+        if (collision.gameObject.layer == _playerLayer)
+        {
+            if (_HP > 0)
+            {
+                _HP--;
+            }
+            else
+            {
+                _collider.enabled = false;
+            }
+        }
+
+        PlayCollisionSound();
+    }
+
+    private void PlayCollisionSound()
+    {
+        _sound.Play();
     }
 }
